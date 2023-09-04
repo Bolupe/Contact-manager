d14c128ca462feae27d93af47b41a0e2e03fd6eb
Bolupe Taiwo <bolupetaiwo15@gmail.com>
2023-09-04 15:53:46 +0100

    properly documenting code, Contact card.

--- contact-app/src/components/ContactCard.js
+++ contact-app/src/components/ContactCard.js
@@ -3,11 +3,17 @@ import { Link } from "react-router-dom";
 import { useContactsCrud } from "../context/ContactsCrudContext";
 import user from "../images/user.png";
 
+// this component is used to display contact information as a card. It provides options to view the contact details, edit the contact, and delete the contact.
+
 const ContactCard = (props) => {
+  // is a functional component that receives a props object as an argument, which is expected to contain contact information like id, name, and email.
   const { id, name, email } = props.contact;
 
   const {removeContactHandler} = useContactsCrud();
 
+  // The deleteContact function, when called, invokes the removeContactHandler function from the context with the contact's id as an argument.
+  // This function is responsible for deleting the contact.
+ 
   const deleteContact = (id) => {
     removeContactHandler(id);
   }
@@ -15,7 +21,10 @@ const ContactCard = (props) => {
   return (
     <div className="item">
       <img className="ui avatar image" src={user} alt="user" />
+      {/* It displays a user avatar image (user.png) on the left side of the contact card. */}
       <div className="content">
+
+      {/* creates a link to the contact's details page (/contact/${id}). It also passes the contact information as state data using the state prop. */}
         <Link
           to = {`/contact/${id}`}
           state={{contact: props.contact}} 
@@ -24,11 +33,15 @@ const ContactCard = (props) => {
           <div>{email}</div>
         </Link>
       </div>
+
+      {/*  When the delete icon is clicked, it triggers the removeContactHandler function from the context to delete the contact from the list of contacts.  */}
       <i
         className="trash alternate outline icon"
         style={{ color: "red", marginTop: "7px", marginLeft: "10px" }}
         onClick={() => deleteContact(id)}
-      ></i>
+      ></i> 
+      
+      {/* provides a link to the edit page with the contact's information as state data. */}
       <Link 
       to={`/edit`}
       state={{ contact: props.contact } }>
