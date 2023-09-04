243961ec1e37b36cf4b2231fdb4f1ecdfe7789cb
Bolupe Taiwo <bolupetaiwo15@gmail.com>
2023-09-04 17:35:06 +0100

    properly documenting code, Add Contact.

--- contact-app/src/components/AddContact.js
+++ contact-app/src/components/AddContact.js
@@ -3,14 +3,23 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { useContactsCrud } from "../context/ContactsCrudContext";
 
+
+// This component allows users to input a new contact's name and email, and upon submission, it sends a POST request to add the contact to the server's database running locally. 
+// It also updates the contact list on the client side by calling the addContactHandler function from the context.
+
 const AddContact = () => {
+  // These states will be used to capture the user's input for the contact's email and name.
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
 
+  // This function will be used to add the new contact to the list.
   const { addContactHandler } = useContactsCrud();
+
+  // used to redirect the user to the contact list page ("/") after successfully adding a contact.
   const navigate = useNavigate();
 
   const add = async (e) => {
+    // to avoid a page refresh.
     e.preventDefault();
     if (name === "" || email === "") {
       alert("All the fields are mandatory!");
@@ -21,11 +30,14 @@ const AddContact = () => {
       // Send a POST request to add the new contact to the database
       const response = await axios.post("http://localhost:3006/contacts", { name, email });
       const newContact = response.data; // Assuming the API returns the new contact
+      
+      // add the new contact to the list of contacts.
       addContactHandler(newContact);
       setEmail("");
       setName("");
       navigate("/");
     } catch (error) {
+      // catches and logs any errors that may occur during the process.
       console.error("Error adding contact:", error);
     }
   };
@@ -33,6 +45,8 @@ const AddContact = () => {
   return (
     <div className="ui main">
       <h2>Add Contact</h2>
+      {/* a form with two input fields for name and email, and a "Add" button.
+       When the form is submitted, it triggers the add function. */}
       <form className="ui form" onSubmit={add}>
         <div className="field">
           <label>Name</label>
