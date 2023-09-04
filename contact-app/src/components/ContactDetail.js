a59119b16daa0a768dc88f41ea46cdeaf6e27bd6
Bolupe Taiwo <bolupetaiwo15@gmail.com>
2023-09-04 16:15:11 +0100

    properly documenting code, Contact Detail.

--- contact-app/src/components/ContactDetail.js
+++ contact-app/src/components/ContactDetail.js
@@ -2,8 +2,11 @@ import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import user from "../images/user.jpg";
 
+// This component is responsible for displaying detailed contact information, including the contact's image, name, and email. It also provides a button to navigate back to the contact list page.
+
 const ContactDetail = () => {
 
+// The useLocation hook is used to access the location object, and the contact information is extracted from location.state.contact.
   const location = useLocation();
   const {name, email} = location.state.contact;
 
@@ -19,6 +22,8 @@ const ContactDetail = () => {
         </div>
       </div>
       <div className="center-div">
+
+        {/* The "Back to Contact List" button is a Link component that directs the user back to the contact list page ("/") when clicked. */}
         <Link to="/">
           <button className="ui button blue center">
             Back to Contact List
